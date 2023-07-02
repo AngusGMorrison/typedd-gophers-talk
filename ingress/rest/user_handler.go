b475dfab43ca62d7211ca0b86f8911919ac4a856
AngusGMorrison <github@angus-morrison.com>
2023-07-02 12:10:15 +0100

    Initial domain and handler sketch

--- ingress/rest/user_handler.go
+++ ingress/rest/user_handler.go
@@ -1,1 +1,51 @@
 package rest
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/angusgmorrison/typeddtalk/domain"
+	"net/http"
+)
+
+// userHandler handles all requests to /users.
+type userHandler struct {
+	service domain.UserService
+}
+
+type createUserRequestBody struct {
+	Email    string
+	Password string
+	Bio      string
+}
+
+type createUserResponseBody struct {
+	ID string `json:"id"`
+}
+
+func (uh *userHandler) create(w http.ResponseWriter, r *http.Request) {
+	var reqBody createUserRequestBody
+	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	_ = r.Body.Close()
+
+	user, err := uh.service.Create(reqBody.Email, reqBody.Password, reqBody.Bio)
+	if err != nil {
+		if errors.Is(err, &domain.InvalidUserError{}) {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		panic(err) // let the recovery middleware handle this
+	}
+
+	resBody := createUserResponseBody{ID: user.ID.String()}
+	b, err := json.Marshal(&resBody)
+	if err != nil {
+		panic(err)
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	_, _ = w.Write(b)
+}
