64c7386b11ddfdeb49d772ed5b1a24990e22acc6
AngusGMorrison <github@angus-morrison.com>
2023-07-11 19:44:38 +0100

    Correct Option implementation

--- pkg/typedd/option.go
+++ pkg/typedd/option.go
@@ -1,15 +1,22 @@
 package typedd
 
-// Option represents a value that must be complete if it is present, but may be absent.
-type Option[T Complete] struct {
+import "reflect"
+
+// Option represents an optional value.
+// The zero-value of T ALWAYS considered valid, and is typically used to indicate the removal of an existing value.
+// Where T: [Complete], partially complete values are considered invalid.
+type Option[T any] struct {
 	value T
 	some  bool
 }
 
-// Some returns an Option[T] containing the given value if it is complete, or an error if it is incomplete.
-func Some[T Complete](value T) (Option[T], error) {
-	if !value.Complete() {
-		return Option[T]{}, &IncompleteTypeError{Incomplete: value} // error at instantiation
+// Some returns an Option[T] containing the given value, which may be T's zero value.
+// Where T: [Complete], invoking Some with a partially-complete value returns [IncompleteTypeError].
+func Some[T any](value T) (Option[T], error) {
+	if complete, ok := any(value).(Complete); ok {
+		if partiallyComplete(complete) {
+			return Option[T]{}, &IncompleteTypeError{Incomplete: complete}
+		}
 	}
 
 	return Option[T]{
@@ -18,7 +25,7 @@ func Some[T Complete](value T) (Option[T], error) {
 	}, nil
 }
 
-// None returns an empty, zero-valued for Option[T]. That is, the zero-value for Option is a valid value.
+// None returns an empty, zero-valued for Option[T]. Hence, the zero-value of Option is always valid.
 func None[T Complete]() Option[T] {
 	return Option[T]{}
 }
@@ -26,4 +33,8 @@ func None[T Complete]() Option[T] {
 // Value returns the value of the Option[T] and true if it is Some, or T's zero-value and false if it is None.
 func (o *Option[T]) Value() (T, bool) {
 	return o.value, o.some
+}
+
+func partiallyComplete(maybePartial Complete) bool {
+	return !reflect.ValueOf(maybePartial).IsZero() && !maybePartial.Complete()
 }
